9db1bd2e7c9372c83fa7aa3d9c62ffe0bcd64225
jordan204 <jordantoutsop4720yahoo.fr>
2020-11-27 15:53:04 +0100

    test avant le auth-guard

--- src/app/_services/user.ts
+++ src/app/_services/user.ts
@@ -2,6 +2,6 @@ export class User {
     id: number;
     username: string;
     password: string;
-    email_address: string;
-    phone_number: string;
+    email: string;
+    phoneNumber: string;
 }
